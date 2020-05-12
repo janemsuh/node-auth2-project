4ec93792abd0e706e2c4493726ebc90011afd5e0
Jane Suh <sourcherryyogurt@gmail.com>
2020-05-12 19:58:35 -0400

    Added register-router.js

---
+++ routers/register-router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const Users = require('../models/users-model');
+
+const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+    try {
+        const username = req.body.username;
+        const user = await Users.findBy('username', username).first();
+        if (user) {
+            return res.status(409).json({
+                message: 'Username is already taken'
+            });
+        }
+        // password is hashed before insertion within the add function
+        res.status(201).json(await Users.add(req.body));
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = router;
