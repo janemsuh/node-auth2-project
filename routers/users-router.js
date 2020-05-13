9dbd0321a057144b72c9125aa2b9150866d49860
Jane Suh <sourcherryyogurt@gmail.com>
2020-05-12 20:04:33 -0400

    Added users-router.js

---
+++ routers/users-router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const Users = require('../models/users-model');
+const restricted = require('../middleware/restricted');
+
+const router = express.Router();
+
+router.get('/', restricted('admin'), async (req, res, next) => {
+    try {
+        res.json(await Users.find());
+    } catch (err) {
+        next(err);
+    }
+})
+
+module.exports = router;
